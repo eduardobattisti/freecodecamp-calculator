a7fdb460e392b80ac9a3a0cdbb2aa1ec6783a6bd
eduardobattisti <eduardobattisti10@gmail.com>
2021-05-03 12:56:29 -0300

    add buttons, +, -, /, = and AC. Update the grid to looks like a calculator

--- src/containers/calculator.jsx
+++ src/containers/calculator.jsx
@@ -10,11 +10,21 @@ const Calculator = () => {
 	const numberButtons = [];
 
 	for (let i = 0; i < 10; i += 1) {
-		numberButtons.push(<Button id={i} value={i} className={i === 0 ? 'zeroButton' : 'defaultButton'} />);
+		numberButtons.push(<Button id={i} value={i} className={i === 0 ? 'horizontalButton' : 'defaultButton'} />);
+		if (i === 3) {
+			numberButtons.push(<Button id="subtraction" value="-" className="defaultButton" />);
+		} else if (i === 6) {
+			numberButtons.push(<Button id="addition" value="+" className="defaultButton" />);
+		} else if (i === 9) {
+			numberButtons.push(<Button id="equal" value="=" className="verticalButton" />);
+		}
 	}
 
 	return (
 		<div className="calculator">
+			<Button id="clear" value="AC" className="horizontalButton" />
+			<Button id="divide" value="/" className="defaultButton" />
+			<Button id="multiply" value="X" className="defaultButton" />
 			{numberButtons}
 			<Button id="decimal" value="." className="defaultButton" />
 		</div>
